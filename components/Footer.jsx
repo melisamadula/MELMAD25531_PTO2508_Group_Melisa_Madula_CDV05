c1a9fdedf839589d59e8a29b669e9fb95c6daa2a
melisamadula <melisamadula.mm@gmail.com>
2026-06-13 18:36:15 +0200

    Started on footer: adding links and styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,15 @@
 export default function Footer() {
     return (
         <footer>
-            <p>All rights reserved.</p>
+            <div className="footer-content">
+                <p className="footer-text">Connect with me:</p>
+                <a href="https://github.com/melisamadula">
+                    <img className="footer-icon" src="./images/github-logo.png" alt="GitHub logo" />
+                </a>
+                <a href="https://www.linkedin.com/in/melisa-madula-24a8213b5/">
+                    <img className="footer-icon" src="./images/LinkedIn-logo.png" alt="LinkedIn logo" />
+                </a>
+            </div>
         </footer>
     )
 }
