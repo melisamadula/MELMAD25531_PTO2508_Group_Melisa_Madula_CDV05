0eb6096e830ce98a28e406349afd927d616f516e
melisamadula <melisamadula.mm@gmail.com>
2026-06-13 20:07:05 +0200

    Deleted data.js and reconfigured my other files for the new structure

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,16 +1,17 @@
-export default function Entry(props) {
+export default function Entry() {
     return (
         <article className="website-entry">
             <div className="main-image-container">
                 <img 
                     className="main-image"
-                    src={props.img.src}
-                    alt={props.img.alt}
+                    src= "./images/Profile picture.jpg"
+                    alt= "Picture of Melisa Madula"
                 />
             </div>
             <div className="info-container">
-                <h2 className="entry-title">{props.title}</h2>
-                <p className="entry-text">{props.text}</p>
+                <h1 className="entry-title">MELISA MADULA</h1>
+                <p className="entry-text">I am a software developer with a passion for creating innovative and efficient solutions. With a strong background in programming and problem-solving, I am dedicated to delivering high-quality code and continuously improving my skills. I thrive in collaborative environments and enjoy working on projects that challenge me to think critically and creatively.</p>
+                <p className="entry-text">My resume will give you a comprehensive overview of my skills and experience as a software developer. It highlights my technical expertise, project accomplishments, and professional background. You can view my resume by clicking the linked Google docs icon below in the footer.</p>
             </div> 
         </article>
     )
