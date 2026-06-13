08b6e6e76c913482aae3db380547c79cd1d5c0b1
melisamadula <melisamadula.mm@gmail.com>
2026-06-13 22:10:45 +0200

    Added contact image icons in the footer, that are clickable links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,6 +12,12 @@ export default function Footer() {
                 <a href="https://docs.google.com/document/d/1ZQeiCOFTdVsaa9KKMGg34nFQWjP8bR4m5qTi23XMj6A/edit?usp=sharing">
                     <img className="footer-icon" src="./images/google-docs.png" alt="Google Docs logo" />
                 </a>
+                <a href="mailto:melisa.madula.mm@gmail.com">
+                    <img className="footer-icon" src="./images/email-logo.png" alt="Email logo" />
+                </a>
+                <a href="tel:+27665496967">
+                    <img className="footer-icon" src="./images/phone-logo.jpeg" alt="Phone logo" />
+                </a>
             </div>
         </footer>
     )
