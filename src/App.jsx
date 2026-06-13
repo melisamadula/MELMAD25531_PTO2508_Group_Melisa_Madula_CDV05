44b5cfa858bee99167bebfb2a7d96bdf3e139349
melisamadula <melisamadula.mm@gmail.com>
2026-06-13 17:40:30 +0200

    Debugged and Tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Header from "./components/Header"
-import Entry from "./components/Entry"
-import Footer from "./components/Footer"
-import data from "./src/data"
+import Header from "../components/Header"
+import Entry from "../components/Entry"
+import Footer from "../components/Footer"
+import data from "./data"
 
 export default function App() {
     
